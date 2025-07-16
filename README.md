8ca2d220c3f3e961365cdcce91f2864aa2667ede
dieosurya <dieosurya@gmail.com>
2025-07-16 12:18:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-shadcn-app
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}", // penting untuk Javascript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
