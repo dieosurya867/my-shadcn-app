25c3696811dab7c728180a82bf30062b36c5206c
dieosurya <dieosurya@gmail.com>
2025-08-05 19:01:21 +0700

    4 cara conditional rendering

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,24 @@
+// conditional rendering dengan component
+const ArticleStatus = ({ isNew }) => {
+  return isNew && <span>== Baru !!!</span>;
+};
+
+// pengecekan nilai conditional rendering diluar component
+const NewArticle = () => {
+  return <span>== Baru !!</span>;
+};
+
 function Blog(props) {
   return (
     <>
       <h3>{props.title}</h3>
       <small>
-        Date: {props.date} tags: {props.tags.join(", ")}
+        Date: {props.date} tags: {props.tags.join(", ")}{" "}
+        {/* conditional rendering */}
+        {/* {props.isNew ? "-- Baru !!" : "Postingan Lama"} */}
+        {/* {props.isNew && "-- Baru !!"} */}
+        {/* <ArticleStatus isNew={props.isNew} /> */}
+        {props.isNew && <NewArticle />}
       </small>
     </>
   );
