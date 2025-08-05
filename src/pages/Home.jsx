442c9feb516f9df3db242fe9c92cccfd7bdd7b7f
dieosurya <dieosurya@gmail.com>
2025-08-05 14:59:19 +0700

    pengenalan useState

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
+import { useState } from "react";
+
 import Article from "../components/Article";
 import Blog from "../components/Blog";
 import posts from "../posts.json";
 
 function Home() {
+  const [search, setSearch] = useState("");
+
+  const changeSearch = (event) => {
+    // console.log(event.target.value);
+    setSearch(event.target.value);
+  };
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog, index) => (
-        <Blog
-          key={index}
-          title={blog.title}
-          tags={blog.tags}
-          date={blog.date}
-        />
+      <div>
+        Cari Artikel : <input onChange={changeSearch} type="text" />
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {/* change yg awal nya memasang props menjadi destructuring */}
+      {posts.map(({ title, tags, date }, index) => (
+        // <Blog key={index} title={title} tags={tags} date={date} />
+        <Blog {...{ title, tags, date }} key={index} />
       ))}
     </>
   );
