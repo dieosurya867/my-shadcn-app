4ba3ab44d0553f83425fbf50b4fee8f731c74bbf
dieosurya <dieosurya@gmail.com>
2025-08-07 19:03:52 +0700

    Dynamic parameter route dan menampilkan artikel

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home";
 import Blog from "../pages/blogs/index";
+import Post from "../pages/blogs/_id";
 import About from "../pages/About";
 
 export const router = createBrowserRouter([
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
       {
         path: "/blog",
         element: <Blog />,
+      },
+      {
+        path: "/blog/:id",
+        element: <Post />,
       },
       {
         path: "/about",
